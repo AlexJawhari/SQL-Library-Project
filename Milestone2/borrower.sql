120e106ba8f645045467d4cb120b9b00da7a680d
Samuel <samuelsay86@gmail.com>
2025-12-09 18:31:38 -0600

    Rename card_id to card_no in borrower table

--- Milestone2/borrower.sql
+++ Milestone2/borrower.sql
@@ -1,6 +1,6 @@
 --1. Create borrower table
 CREATE TABLE Borrower (
-     card_id VARCHAR(20) PRIMARY KEY,
+     card_no VARCHAR(20) PRIMARY KEY,
      ssn VARCHAR(11) NOT NULL UNIQUE,
      bname VARCHAR(100) NOT NULL,
      address VARCHAR(255) NOT NULL,
@@ -19,13 +19,13 @@ CREATE PROCEDURE create_borrower(
      IN p_bname VARCHAR(100),
      IN p_address VARCHAR(255),
      IN p_phone VARCHAR(20),
-     OUT p_card_id VARCHAR(20)
+     OUT p_card_no VARCHAR(20)
 )
 
 BEGIN
-     DECLARE v_max_card_id VARCHAR(20);
+     DECLARE v_max_card_no VARCHAR(20);
      DECLARE v_next_num INT;
-     DECLARE v_new_card_id VARCHAR(20);
+     DECLARE v_new_card_no VARCHAR(20);
 
      --validation of attributes
 
@@ -59,25 +59,25 @@ BEGIN
      END IF;
 
 
-     --generate new card_id compatible with existing IDs
+     --generate new card_no compatible with existing IDs
 
-     SELECT MAX(card_id)
-     INTO v_max_card_id
+     SELECT MAX(card_no)
+     INTO v_max_card_no
      FROM Borrower;
 
-     IF v_max_card_id IS NULL THEN
+     IF v_max_card_no IS NULL THEN
           SET v_next_num = 1;
      ELSE
-           SET v_next_num = CAST(SUBSTRING(v_max_card_id, 3) AS UNSIGNED) + 1;
+           SET v_next_num = CAST(SUBSTRING(v_max_card_no, 3) AS UNSIGNED) + 1;
      END IF;
      
 
-     SET v_new_card_id = CONCAT('ID', LPAD(v_next_num, 6, '0'));
+     SET v_new_card_no = CONCAT('ID', LPAD(v_next_num, 6, '0'));
      
-     INSERT INTO Borrower (card_id, ssn, bname, address, phone)
-     VALUES (v_new_card_id, p_ssn, p_bname, p_address, p_phone);
+     INSERT INTO Borrower (card_no, ssn, bname, address, phone)
+     VALUES (v_new_card_no, p_ssn, p_bname, p_address, p_phone);
 
-     SET p_card_id = v_new_card_id;
+     SET p_card_no = v_new_card_no;
 
 END //
 DELIMITER ;
@@ -85,16 +85,16 @@ DELIMITER ;
 
 
 --Example for test
---SET @new_card_id = NULL;
+--SET @new_card_no = NULL;
 --CALL create_borrower(
 --     '123-45-6789',
 --     'Greg John',
 --     '123 Utd St, Richardson, TX',
 --     '323-415-6923',
---     @new_card_id
+--     @new_card_no
 --);
 
---SELECT @new_card_id AS created_card_id;
+--SELECT @new_card_no AS created_card_no;
 
 --duplicate ssn which should cause an error
 --CALL create_borrower(
@@ -102,6 +102,6 @@ DELIMITER ;
 --     'Gregory John',
 --     '321 Utd St, Richardson, TX',
 --     '123-414-2323',
---     @new_card_id
+--     @new_card_no
 --);
      
